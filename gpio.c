a62164c350f66dd18a44464c46d33e1bdabff652
Kanjana Eiamsaard <kanjana@eng.src.ku.ac.th>
2019-10-24 02:26:14 +0700

    remove bad gpio.c

--- gpio.c
+++
@@ -1,8 +0,0 @@
-#include<linux/kernel.h>
-#include<linux/module.h>
-#include<linux/moduleparam.h>
-#include<linux/init.h>
-#include<linux/proc_fs.h>
-#include<asm/uaccess.h>
-#define alpha "abcdefghijklmnopqrstuvwxyz"
-#define BUFSIZE 32
